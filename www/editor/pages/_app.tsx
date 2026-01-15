4642d394bd8eb6658430f8f45b5b95646cb12f13
Dan Lynch <pyramation@gmail.com>
2026-01-14 18:09:32 -0800

    fix

--- www/editor/pages/_app.tsx
+++ www/editor/pages/_app.tsx
@@ -1,30 +1,6 @@
-// @ts-nocheck
 import "../styles/globals.css";
-import "@interchain-ui/react/styles";
-
 import type { AppProps } from "next/app";
-import {
-  Box,
-  ThemeProvider,
-  useColorModeValue,
-  useTheme,
-} from "@interchain-ui/react";
 
-function CreateCosmosApp({ Component, pageProps }: AppProps) {
-  const { themeClass } = useTheme();
-
-  return (
-    <ThemeProvider>
-      <Box
-        className={themeClass}
-        minHeight="100dvh"
-        backgroundColor={useColorModeValue("$white", "$background")}
-      >
-        {/* @ts-ignore */}
-        <Component {...pageProps} />
-      </Box>
-    </ThemeProvider>
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
-
-export default CreateCosmosApp;
